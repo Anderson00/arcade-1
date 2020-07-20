b9f5eb0f85b2091a2136ff2d308b56c2d39a00de
Anderson <anderson.asm@alu.ufc.br>
2020-07-19 21:13:58 -0300

    test

---
+++ Readme.md
@@ -0,0 +1,36 @@
+
+## Links
+- [init](#init)
+- [basic](#basic)
+- [noobie](#noobie)
+- [rookie](#rookie)
+- [fighter](#fighter)
+- [jedi](#jedi)
+
+## init
+
+- [@000 Shell](base/000/Readme.md#00_init---shell-sub-entendendo-como-funciona-a-main) [Entendendo como funciona a main]
+
+## basic
+
+- [@001 Calculadora](base/001/Readme.md#01_basic--calculadora-sub-um-modelo-persistente-com-uma-classe) [Um modelo persistente com uma classe]
+- [@002 Carro](base/002/Readme.md#01_basic--carro-sub-uma-classe-e-muitas-ações) [Uma classe e muitas ações]
+
+## noobie
+
+- [@004 Lapiseira e Grafite](base/004/Readme.md#02_noobie-lapiseira-e-grafite-sub-duas-classes-agregação-null) [Duas classes, agregação, null]
+- [@003 Motoca de aluguel](base/003/Readme.md#02_noobie-motoca-de-aluguel-sub-duas-classes-agregação-nulo) [Duas classes, agregação, nulo]
+
+## rookie
+
+- [@005 Mestre dos Vetores](base/005/Readme.md#03_rookie-mestre-dos-vetores-sub-arraylist-e-vector) [ArrayList e Vector]
+- [@009 Parquinho: Hora do pula pula](base/009/Readme.md#03_rookie-parquinho-hora-do-pula-pula-sub-agregação-vetores-e-implementação-livre) [agregação, vetores e implementação livre]
+- [@006 Tamagotchi - Seu Pet Virtual](base/006/Readme.md#03_rookie-tamagotchi---seu-pet-virtual-sub-tostring-getters-and-setters) [toString, getters and setters]
+
+## fighter
+
+- [@007 Agencia Bancária 1](base/007/Readme.md#05_fighter-agencia-bancária-1)
+
+## jedi
+
+- [@008 Agiota Gente Fina](base/008/Readme.md#06_jedi-agiota-gente-fina-arrays-mapas-crud)
---
+++ base/003/Solver.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+class Pessoa {
+    String nome;
+    int idade;
+
+    public Pessoa(String nome, int idade){ //nao eh metodo, eh invocado automaticaticamente na criacao
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String toString(){
+        return "[" + this.nome + ":" + this.idade + "]";
+    }
+}
+
+class Motoca {
+	Pessoa pessoa; //agregacao
+    int potencia;
+    int tempo;
+
+	public Motoca(int potencia){
+        this.potencia = potencia;
+        this.tempo = 0;
+	}
+	~Motoca(){
+		if(pessoa)
+			delete pessoa;
+	}
+    
+    void comprar(int tempo){
+        this.tempo += tempo;
+    }
+	
+	bool embarcar(Pessoa * pessoa) {
+		if(this.pessoa == null){
+			this.pessoa = pessoa;
+			return true;
+		}
+		System.out.println("fail: moto ocupada");
+		return false;
+	}
+	
+	Pessoa * desembarcar() {
+		if(this.pessoa != nullptr){
+			Pessoa * pessoa = this.pessoa;
+			this->pessoa = null;
+			return pessoa;
+		}else{
+			System.out.println("fail: moto vazia");
+			return nullptr;
+		}
+	}
+
+	void dirigir(int tempo){
+		if(this.pessoa == null)
+			System.out.println("fail: moto vazia");
+		else if(this.pessoa.idade > 10)
+			System.out.println("fail: muito grande para andar de moto");
+        else if(this.tempo == 0)
+			System.out.println("fail: tempo zerado");
+        else if(this.tempo < tempo){
+            System.out.println("fail: andou " + this.tempo + " min e acabou o tempo");
+            this.tempo = 0;
+        }else
+            this.tempo -= tempo;
+    }
+
+	void buzinar(){
+        if(this.pessoa == null){
+            System.out.println("fail: moto vazia");
+            return;
+        }
+		String saida = "";
+		for(int i = 0; i < this.potencia; i++)
+			saida += "e";
+		System.out.println("P" + saida + "m");
+    }
+    
+    public String toString(){
+        return "potencia: " + this.potencia + ", minutos: " + this.tempo + ", pessoa: " + this.pessoa;
+    }
+}
+
+public class Solver{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Motoca motoca  = new Motoca(1);
+		while(true) {
+			String line = scanner.nextLine();
+            String ui[] = line.split(" "); //ui user input eh um vetor de strings
+            System.out.println("$" + line);
+			if(ui[0].equals("end")) {
+				break;
+			}else if(ui[0].equals("init")) { //potencia
+				Pessoa pessoa = motoca.pessoa;
+				motoca = new Motoca(Integer.parseInt(ui[1]));
+				motoca.embarcar(pessoa);
+			}else if(ui[0].equals("in")) { //in nome idade
+				int idade = Integer.parseInt(ui[2]);
+				Pessoa * pessoa = new Pessoa(ui[1], idade);
+				if(!motoca.embarcar(pessoa))
+					delete pessoa;
+			}else if(ui[0].equals("out")) {
+				Pessoa * pessoa = motoca.desembarcar();
+				if(pessoa)
+					delete pessoa;
+			}else if(ui[0].equals("show")) {
+				System.out.println(motoca);
+			}else if(ui[0].equals("drive")) {
+				motoca.dirigir(Integer.parseInt(ui[1]));
+			}else if(ui[0].equals("buy")) {
+				motoca.comprar(Integer.parseInt(ui[1]));
+			}else if(ui[0].equals("buzinar")) {
+				motoca.buzinar();
+			}else {
+				System.out.println("Comando invalido");
+			}
+		}
+		delete motoca;
+		scanner.close();
+	}
+}
+
+
+
+
+
+
